4fe47797b5150d82a94a27c86a4da3a3d70d3da0
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-04-05 19:34:56 +0900

    タイマー追加

--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useCallback, useEffect, useState } from 'react';
 import QRCode from '../components/QRCode';
 import User from '../types/User';
 import { BACKEND_ENDPOINT } from '../const';
@@ -8,14 +8,17 @@ type UserPageProps = {
   user: User;
 };
 
+const QR_MAX_TIMEOUT_SEC = 300;
+
 const UserPage = (props: UserPageProps) => {
-  const [qr, setQR] = useState<ReactElement>();
+  const [qr, setQR] = useState<ReactElement | null>(null);
+  const [count, setCount] = useState<number>(QR_MAX_TIMEOUT_SEC);
 
   const onClick = () => {
     createTicket();
   };
 
-  const createTicket = async () => {
+  const createTicket = useCallback(async () => {
     const payload = {
       user_id: props.user.id
     };
@@ -35,17 +38,51 @@ const UserPage = (props: UserPageProps) => {
     if (res.ok) {
       GenerateQRCode(data.uuid);
     }
-  };
+  }, [props.user.id]);
 
   function GenerateQRCode(data: string) {
-    setQR(<QRCode data={data}></QRCode>);
+    setCount(QR_MAX_TIMEOUT_SEC);
+
+    setQR(
+      <>
+        <QRCode data={data} />
+      </>
+    );
   }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (count <= 0) {
+        createTicket();
+      } else {
+        setCount(count - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [count, createTicket]);
+
+  const sec = count % 60;
+  const min = (count - sec) / 60;
 
   return (
     <div className="m-10">
       <h1>BUTSURY DAYS</h1>
       <button onClick={onClick}>Press to generate QR code</button>
       {qr}
+      {qr ? (
+        <>
+          <p>
+            有効期限: {min}分{sec.toString().padStart(2, '0')}秒
+          </p>
+          <button
+            onClick={() => {
+              setQR(null);
+            }}>
+            X
+          </button>
+        </>
+      ) : null}
     </div>
   );
 };
