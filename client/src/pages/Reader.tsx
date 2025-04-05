82b4d66be7aefb71762bc368158584eac1505727
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-04-05 19:45:01 +0900

    見た目の調整

--- client/src/pages/Reader.tsx
+++ client/src/pages/Reader.tsx
@@ -22,7 +22,7 @@ const ReaderPage = () => {
   };
 
   return (
-    <>
+    <div className='bg-black'>
       <div className="m-auto h-screen w-screen max-w-[400px] flex flex-col">
         <p className="bg-blue-400 text-white font-bold shrink">乗降兼用</p>
         <div className="bg-gray-500 grow">
@@ -32,22 +32,21 @@ const ReaderPage = () => {
                 <td className="p-1 text-start">精算額</td>
                 <td className='p-1 text-end after:content-["円"]'>1,000</td>
               </tr>
-              <tr>
+              <tr className='bg-blue-400'>
                 <td className="p-1 text-start ">残高</td>
                 <td className='p-1 text-end after:content-["円"]'></td>
               </tr>
               <tr>
                 <td className="p-1 text-start">
-                  定期券
-                  <br />
-                  有効期限
+                  定期券 有効期限
                 </td>
                 <td className="p-1 text-end">2025年11月4日</td>
               </tr>
             </tbody>
           </table>
         </div>
-        <div className="bg-black border-2 shrink">
+        <div className="h-[8%] bg-white"></div>
+        <div className="bg-gray-900 shrink">
           <Scanner
             onScan={handleScan}
             formats={['qr_code', 'micro_qr_code']}
@@ -63,7 +62,7 @@ const ReaderPage = () => {
           />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
