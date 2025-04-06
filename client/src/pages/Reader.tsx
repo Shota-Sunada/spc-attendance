2eefeb767a216b7a0ef4eed329b8b1d7756ed4fc
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-04-06 10:09:05 +0900

    ちょっと変更

--- client/src/pages/Reader.tsx
+++ client/src/pages/Reader.tsx
@@ -6,11 +6,7 @@ import Ticket from '../types/Ticket';
 import User from '../types/User';
 import { useSearchParams } from 'react-router-dom';
 
-type ReaderStatus = {
-  status: 'getOn' | 'getOff' | 'standby' | 'standby-getOn' | 'standby-getOff';
-  is_reading: false;
-};
-
+type ReaderStatus = 'getOn' | 'getOff' | 'standby' | 'standby-getOn' | 'standby-getOff' | 'isReading';
 type ReaderMode = 'getOn' | 'getOff' | 'getOnOff' | null;
 
 const ReaderPage = () => {
@@ -20,6 +16,7 @@ const ReaderPage = () => {
   const [lastUUID, setLastUUID] = useState<string | null>(null);
   const [headerText, setHeaderText] = useState<string>('兼用');
   const [headerCss, setHeaderCss] = useState<string>('text-blue-400');
+  const [currentStatus, setCurrentStatus] = useState<ReaderStatus>('standby');
 
   const [params] = useSearchParams();
 
@@ -76,21 +73,24 @@ const ReaderPage = () => {
   );
 
   const tableBottomWelcome = (
-    <tr>
-      {/* <td className="reader-left">
-        <p className="reader-text reader-text-left">有効期限</p>
+    <tr className="bg-gray-600">
+      <td className="reader-left" colSpan={2}>
+        <p className="reader-text text-center text-2xl text-green-500">QRコードをかざしてください。</p>
       </td>
-      <td className="reader-right">
-        <p className="reader-text reader-text-right">
-          2025<span className="text-2xl">年</span>11<span className="text-2xl">月</span>4<span className="text-2xl">日</span>
-        </p>
-      </td> */}
+    </tr>
+  );
+
+  const tableBottomWait = (
+    <tr className="bg-green-400">
+      <td className="reader-left" colSpan={2}>
+        <p className="reader-text text-center text-2xl text-gray-800">しばらくお待ち下さい。</p>
+      </td>
     </tr>
   );
 
   const tableNullRow = (
-    <tr>
-      <td className="reader-left">
+    <tr className="">
+      <td className="reader-left border-r-[1px] border-gray-500">
         <p className="reader-text reader-text-left">{'　'}</p>
       </td>
       <td className="reader-right">
@@ -100,13 +100,8 @@ const ReaderPage = () => {
   );
 
   const [tableTop, setTableTop] = useState<ReactElement>(tableTopNumber);
-  const [tableMiddle, setTableMiddle] = useState<ReactElement>(tableMiddleBalance);
-  const [tableBottom, setTableBottom] = useState<ReactElement>(tableBottomLimit);
-
-  const currentStatus: ReaderStatus = {
-    status: 'standby',
-    is_reading: false
-  };
+  const [tableMiddle, setTableMiddle] = useState<ReactElement>(tableNullRow);
+  const [tableBottom, setTableBottom] = useState<ReactElement>(tableBottomWelcome);
 
   const setReaderMode = (): ReaderMode => {
     const mode = params.get('mode');
@@ -125,73 +120,8 @@ const ReaderPage = () => {
 
   const readerMode: ReaderMode = setReaderMode();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLastUUID(null);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, [lastUUID]);
-
-  const handleScan = async (results: IDetectedBarcode[]) => {
-    if (results.length > 0) {
-      setScanResult({
-        format: results[0].format,
-        rawValue: results[0].rawValue
-      });
-
-      if (lastUUID == scanResult.rawValue) return;
-
-      setLastUUID(scanResult.rawValue);
-      const payload = {
-        uuid: scanResult.rawValue
-      };
-
-      console.log(scanResult.rawValue);
-
-      const res = await fetch(`${BACKEND_ENDPOINT}/useTicket`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      });
-
-      const data = (await res.json()) as Ticket;
-      if (res.ok) {
-        console.log('hi!');
-        const payload2 = {
-          id: data.user_id
-        };
-        const res2 = await fetch(`${BACKEND_ENDPOINT}/api/users`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(payload2)
-        });
-
-        const data2 = (await res2.json()) as User;
-        if (res2.ok) {
-          setBalance(data2.balance);
-
-          currentStatus.status = 'getOn';
-          updateDisplay();
-        }
-      }
-    }
-  };
-
-  const customTracker = (detectedCodes: IDetectedBarcode[], ctx: CanvasRenderingContext2D) => {
-    detectedCodes.forEach((code) => {
-      ctx.strokeStyle = 'red';
-      ctx.lineWidth = 3;
-      ctx.strokeRect(code.boundingBox.x, code.boundingBox.y, code.boundingBox.width, code.boundingBox.height);
-    });
-  };
-
   const updateDisplay = () => {
-    switch (currentStatus.status) {
+    switch (currentStatus) {
       case 'getOn':
         setHeaderText('乗車');
         setHeaderCss('bg-blue-400 text-white');
@@ -227,6 +157,9 @@ const ReaderPage = () => {
         setTableMiddle(tableNullRow);
         setTableBottom(tableBottomWelcome);
         break;
+      case 'isReading':
+        setTableBottom(tableBottomWait);
+        break;
       default:
         setHeaderText('ヘッダー取得エラー');
         setHeaderCss('bg-blue-400 text-white');
@@ -237,16 +170,104 @@ const ReaderPage = () => {
     }
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('reset last uuid');
+      setLastUUID(null);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [lastUUID]);
+
+  // useEffect(() => {
+  //   const timer2 = setTimeout(() => {
+  //     console.log('standby');
+  //     setCurrentStatus('standby');
+  //     updateDisplay();
+  //   }, 5000);
+
+  //   return () => clearTimeout(timer2);
+  // }, [updateDisplay]);
+
+  const handleScan = async (results: IDetectedBarcode[]) => {
+    if (results.length > 0) {
+      setScanResult({
+        format: results[0].format,
+        rawValue: results[0].rawValue
+      });
+
+      if (scanResult.rawValue == lastUUID) return;
+
+      console.log(`rawValue: ${scanResult.rawValue}`);
+      console.log(`lastUUID: ${lastUUID}`);
+
+      setCurrentStatus('isReading');
+      updateDisplay();
+
+      setLastUUID(scanResult.rawValue);
+      const payload = {
+        uuid: lastUUID
+      };
+
+      const res = await fetch(`${BACKEND_ENDPOINT}/useTicket`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+
+      const data = (await res.json()) as Ticket;
+      if (res.ok) {
+        console.log('hi!');
+        const payload2 = {
+          id: data.user_id
+        };
+        const res2 = await fetch(`${BACKEND_ENDPOINT}/api/users`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(payload2)
+        });
+
+        const data2 = (await res2.json()) as User;
+        if (res2.ok) {
+          setBalance(data2.balance);
+
+          setCurrentStatus('getOn');
+          updateDisplay();
+        }
+      }
+    }
+  };
+
+  const customTracker = (detectedCodes: IDetectedBarcode[], ctx: CanvasRenderingContext2D) => {
+    detectedCodes.forEach((code) => {
+      ctx.strokeStyle = 'red';
+      ctx.lineWidth = 3;
+      ctx.strokeRect(code.boundingBox.x, code.boundingBox.y, code.boundingBox.width, code.boundingBox.height);
+    });
+  };
+
   return (
     <div className="bg-black">
       <div className="m-auto h-screen w-screen max-w-[400px] flex flex-col">
         <p className={headerCss + ' pl-1 font-bold shrink'}>{headerText}</p>
-        <div className="bg-gray-500 grow">
+        <div className="bg-gray-700 grow">
           <table className="text-white w-full h-full">
             <tbody className="">
               {tableTop}
               {tableMiddle}
               {tableBottom}
+              <tr className="">
+                <td className="reader-left border-r-[1px] border-gray-500">
+                  <p className="reader-text text-[5px]">{'　'}</p>
+                </td>
+                <td className="reader-right">
+                  <p className="reader-text text-[5px]">{'　'}</p>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
