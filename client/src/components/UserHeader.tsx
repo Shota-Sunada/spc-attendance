1cd70db9a9d6119acbe627ed2631c69c34ea8a17
ShudoPhysicsClubPC <shudophysicsclub@gmail.com>
2025-04-20 00:08:57 +0900

    チャージ画面作成

--- client/src/components/UserHeader.tsx
+++ client/src/components/UserHeader.tsx
@@ -1,4 +1,6 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import User from '../types/User';
+import { FaChevronLeft } from 'react-icons/fa';
 
 type Props = {
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
@@ -7,6 +9,9 @@ type Props = {
 };
 
 const UserHeader = (props: Props) => {
+  const navigate = useNavigate();
+  const loc = useLocation();
+
   const onLogoutClick = () => {
     if (!window.confirm('ログアウトしてもよろしいですか?')) {
       return;
@@ -18,16 +23,29 @@ const UserHeader = (props: Props) => {
   };
 
   return (
-    <div className="bg-white flex flex-row p-[10px] items-center justify-center">
-      <button className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim" onClick={() => props.setIsMenuOpen(!props.isMenuOpen)}>
-        {'メニュー'}
-      </button>
-      {/* <h1 className="p-[10px] cursor-default">{'BUTSURY DAYS'}</h1> */}
-      <img src="/logo.png" alt="LOGO" className='w-[100px]'/>
-      <button className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim" onClick={onLogoutClick}>
-        {'ログアウト'}
-      </button>
-    </div>
+    <>
+      {loc.pathname === '/' ? (
+        <></>
+      ) : (
+        <div
+          className="fixed block left-[10px] m-[5px] top-[10px] p-[8px] rounded-[20px] cursor-pointer hover:bg-[#f9f9fa] bg-white"
+          onClick={() => navigate('/')}>
+          <FaChevronLeft size={'30px'} />
+        </div>
+      )}
+      <div className="bg-white flex flex-row p-[10px] items-center justify-center x-50">
+        <button
+          className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim"
+          onClick={() => props.setIsMenuOpen(!props.isMenuOpen)}>
+          {'メニュー'}
+        </button>
+        {/* <h1 className="p-[10px] cursor-default">{'BUTSURY DAYS'}</h1> */}
+        <img src="/logo.png" alt="LOGO" className="w-[100px]" />
+        <button className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim" onClick={onLogoutClick}>
+          {'ログアウト'}
+        </button>
+      </div>
+    </>
   );
 };
 
--- client/src/pages/Charge.tsx
+++ client/src/pages/Charge.tsx
@@ -1,15 +1,77 @@
-import { FaChevronLeft } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import '../styles/radio.css';
+import MobiryButton from '../components/MobiryButton';
 
 const Charge = () => {
-  const navigate = useNavigate()
+  const RadioChargeCard = (props: { price: number }) => (
+    <div className="flex flex-row rounded-[10px] bg-white m-[8px] ">
+      <input type="radio" name="charge_options" id={props.price + 'yen'} value={props.price} />
+      <label className="flex items-center justify-center w-[100%] h-[100%] min-w-[120px] p-[10px] rounded-[10px]" htmlFor={props.price + 'yen'}>
+        {props.price + '円'}
+      </label>
+    </div>
+  );
+
+  const charge = () => {};
 
   return (
-    <div className='items-center flex h-[44px] justify-center relative'>
-      <div className="absolute block left-[10px] m-[10px] top-[10px] p-[10px] rounded-[20px] cursor-pointer hover:bg-[#f9f9fa] bg-white" onClick={()=> navigate("/")}>
-        <FaChevronLeft size={"50px"}/>
+    <>
+      <div className="flex flex-col x-0">
+        <p className="m-[10px] flex items-center justify-center font-medium">{'チャージ'}</p>
+        <div className="">
+          <div className="flex flex-row items-center justify-center">
+            <div>
+              <p className="font-medium text-[12px]">{'現在の残高'}</p>
+              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066]'>{'10,000'}</p>
+            </div>
+            <div className="m-[10px]">
+              <FaArrowRight size={'30px'} color="#c7d2d5" />
+            </div>
+            <div>
+              <p className="font-medium text-[12px]">{'チャージ後の残高'}</p>
+              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066]'>{'10,000'}</p>
+            </div>
+          </div>
+          <div className="radio-root flex flex-row items-center justify-center">
+            <div className="flex flex-col">
+              <RadioChargeCard price={1000} />
+              <RadioChargeCard price={3000} />
+              <RadioChargeCard price={8000} />
+            </div>
+            <div className="flex flex-col">
+              <RadioChargeCard price={2000} />
+              <RadioChargeCard price={5000} />
+              <RadioChargeCard price={10000} />
+            </div>
+          </div>
+          <div className="flex items-center justify-center mt-[5px]">
+            <div className="rounded-[5px] px-[10px] py-[10px] bg-[#ebebde] text-[15px]">
+              <div className="flex flex-row">
+                <dt className="pr-[20px]">
+                  <span>支払手段:</span>
+                </dt>
+                <dd>
+                  <p>班長のクレジットカード</p>
+                  <p>SHUDO CARD **** **** **** *817</p>
+                  <p>有効期限: 03/34</p>
+                </dd>
+              </div>
+              <div className="flex flex-row">
+                <dt className="pr-[20px]">
+                  <span>{'　決済額:'}</span>
+                </dt>
+                <dd>
+                  <span>0000円</span>
+                </dd>
+              </div>
+            </div>
+          </div>
+          <div className='flex items-center justify-center m-[20px]'>
+            <MobiryButton text={'チャージする'} onClick={charge} />
+          </div>{' '}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
