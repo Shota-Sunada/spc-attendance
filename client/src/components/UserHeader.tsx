d661d75b81475712276afbe8cb696036ad65fca3
ShudoPhysicsClubPC <shudophysicsclub@gmail.com>
2025-04-20 01:17:20 +0900

    読み取り機でヘッダーを非表示

--- client/src/components/UserHeader.tsx
+++ client/src/components/UserHeader.tsx
@@ -24,27 +24,35 @@ const UserHeader = (props: Props) => {
 
   return (
     <>
-      {loc.pathname === '/' ? (
+      {loc.pathname === '/reader' ? (
         <></>
       ) : (
-        <div
-          className="fixed block left-[10px] m-[5px] top-[10px] p-[8px] rounded-[20px] cursor-pointer hover:bg-[#f9f9fa] bg-white"
-          onClick={() => navigate('/')}>
-          <FaChevronLeft size={'30px'} />
-        </div>
+        <>
+          {loc.pathname === '/' ? (
+            <></>
+          ) : (
+            <div
+              className="fixed block left-[10px] m-[5px] top-[10px] p-[8px] rounded-[20px] cursor-pointer hover:bg-[#f9f9fa] bg-white"
+              onClick={() => navigate('/')}>
+              <FaChevronLeft size={'30px'} />
+            </div>
+          )}
+          <div className="bg-white flex flex-row p-[10px] items-center justify-center x-50">
+            <button
+              className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim"
+              onClick={() => props.setIsMenuOpen(!props.isMenuOpen)}>
+              {'メニュー'}
+            </button>
+            {/* <h1 className="p-[10px] cursor-default">{'BUTSURY DAYS'}</h1> */}
+            <img src="/logo.png" alt="LOGO" className="w-[100px]" />
+            <button
+              className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim"
+              onClick={onLogoutClick}>
+              {'ログアウト'}
+            </button>
+          </div>
+        </>
       )}
-      <div className="bg-white flex flex-row p-[10px] items-center justify-center x-50">
-        <button
-          className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim"
-          onClick={() => props.setIsMenuOpen(!props.isMenuOpen)}>
-          {'メニュー'}
-        </button>
-        {/* <h1 className="p-[10px] cursor-default">{'BUTSURY DAYS'}</h1> */}
-        <img src="/logo.png" alt="LOGO" className="w-[100px]" />
-        <button className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim" onClick={onLogoutClick}>
-          {'ログアウト'}
-        </button>
-      </div>
     </>
   );
 };
