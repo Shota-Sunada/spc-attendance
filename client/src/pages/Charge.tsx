6051935bb227e62ffb6e3a8e6b928c6ef393b2db
ShudoPhysicsClubPC <shudophysicsclub@gmail.com>
2025-04-20 10:28:54 +0900

    チャージ簡略化

--- client/src/pages/Charge.tsx
+++ client/src/pages/Charge.tsx
@@ -7,11 +7,6 @@ import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { apiCharge } from '../api';
 
-interface Radio {
-  label: string;
-  value: number;
-}
-
 interface Props {
   user: User | null;
 }
@@ -22,35 +17,8 @@ const Charge = (props: Props) => {
 
   const navigate = useNavigate();
 
-  const radioButtons1: Radio[] = [
-    {
-      label: '1,000円',
-      value: 1000
-    },
-    {
-      label: '3,000円',
-      value: 3000
-    },
-    {
-      label: '8,000円',
-      value: 8000
-    }
-  ];
-
-  const radioButtons2: Radio[] = [
-    {
-      label: '2,000円',
-      value: 2000
-    },
-    {
-      label: '5,000円',
-      value: 5000
-    },
-    {
-      label: '10,000円',
-      value: 10000
-    }
-  ];
+  const radioButtons1: number[] = [1000, 3000, 8000];
+  const radioButtons2: number[] = [2000, 5000, 10000];
 
   const chargeMoney = async () => {
     if (props.user) {
@@ -64,18 +32,18 @@ const Charge = (props: Props) => {
     <>
       <div className="flex flex-col x-0">
         <p className="m-[10px] flex items-center justify-center font-medium">{'チャージ'}</p>
-        <div className="">
+        <div className="flex flex-col items-center justify-center">
           <div className="flex flex-row items-center justify-center">
-            <div>
-              <p className="font-medium text-[12px]">{'現在の残高'}</p>
-              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066]'>{props.user?.balance}</p>
+            <div className="flex flex-col w-[100%]">
+              <p className="font-medium text-[12px] min-w-[104px]">{'現在の残高'}</p>
+              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066] text-right'>{props.user?.balance}</p>
             </div>
-            <div className="m-[10px]">
+            <div className="m-[10px] flex items-center justify-center">
               <FaArrowRight size={'30px'} color="#c7d2d5" />
             </div>
-            <div>
-              <p className="font-medium text-[12px]">{'チャージ後の残高'}</p>
-              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066]'>
+            <div className="flex flex-col w-[100%]">
+              <p className="font-medium text-[12px] min-w-[104px]">{'チャージ後の残高'}</p>
+              <p className='font-semibold text-[30px] after:content-["\5186"] after:text-[14px] text-[#462066] text-right'>
                 {props.user?.balance === undefined ? charge : props.user?.balance + charge}
               </p>
             </div>
@@ -84,19 +52,12 @@ const Charge = (props: Props) => {
             <div className="flex flex-col">
               {radioButtons1.map((x) => (
                 <div key={uuidv4()} className="flex flex-row rounded-[10px] bg-white m-[8px]">
-                  <input
-                    type="radio"
-                    name="charge_options"
-                    id={x.value + 'yen'}
-                    value={x.value}
-                    checked={x.value === charge}
-                    onChange={changeValue}
-                  />
+                  <input type="radio" name="charge_options" id={x + 'yen'} value={x} checked={x === charge} onChange={changeValue} />
                   <label
                     onClick={() => {}}
-                    className="cursor-pointer flex items-center justify-center w-[100%] h-[100%] min-w-[120px] p-[10px] rounded-[10px]"
-                    htmlFor={x.value + 'yen'}>
-                    {x.label}
+                    className="cursor-pointer flex items-center justify-center w-[100%] h-[100%] min-w-[140px] py-[8px] rounded-[10px] font-bold text-[#219bce] after:content-['\5186']"
+                    htmlFor={x + 'yen'}>
+                    {x}
                   </label>
                 </div>
               ))}
@@ -104,19 +65,12 @@ const Charge = (props: Props) => {
             <div className="flex flex-col">
               {radioButtons2.map((x) => (
                 <div key={uuidv4()} className="flex flex-row rounded-[10px] bg-white m-[8px]">
-                  <input
-                    type="radio"
-                    name="charge_options"
-                    id={x.value + 'yen'}
-                    value={x.value}
-                    checked={x.value === charge}
-                    onChange={changeValue}
-                  />
+                  <input type="radio" name="charge_options" id={x + 'yen'} value={x} checked={x === charge} onChange={changeValue} />
                   <label
                     onClick={() => {}}
-                    className="cursor-pointer flex items-center justify-center w-[100%] h-[100%] min-w-[120px] p-[10px] rounded-[10px]"
-                    htmlFor={x.value + 'yen'}>
-                    {x.label}
+                    className="cursor-pointer flex items-center justify-center w-[100%] h-[100%] min-w-[140px] py-[8px] rounded-[10px] font-bold text-[#219bce] after:content-['\5186']"
+                    htmlFor={x + 'yen'}>
+                    {x}
                   </label>
                 </div>
               ))}
