6eb8d12d59b1e620d925f33f6a3fe3eb9f3b6324
Shota Sunada <shota.sunada.dev@gmail.com>
2025-04-24 12:28:18 +0900

    BANされてもログアウトはできるように修正

--- client/src/components/UserHeader.tsx
+++ client/src/components/UserHeader.tsx
@@ -69,15 +69,13 @@ const UserHeader = (props: Props) => {
                   }
                 }}
               />
-              {props.isBanned ? (
-                <></>
-              ) : (
+              {
                 <button
                   className="bg-[#219bce] rounded-[20px] px-[15px] py-[10px] text-white cursor-pointer hover:bg-[#008fc0] anim"
                   onClick={onLogoutClick}>
                   {'ログアウト'}
                 </button>
-              )}
+              }
             </div>
           </div>
         </>
